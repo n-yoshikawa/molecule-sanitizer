8d8ab49d9a97bd7dd3b285d9265d7be231a55729
Naruki Yoshikawa <naruki.yoshikawa@gmail.com>
2021-11-26 22:04:16 -0500

    Add local setting example

---
+++ sanitizer/settings_local.example.py
@@ -0,0 +1,6 @@
+DEBUG = True
+ALLOWED_HOSTS = ['*']
+
+SECRET_KEY = 'secret'
+SOCIAL_AUTH_TWITTER_KEY = 'xxxxxxxxxxxxxxxxxxxxxxxxx'
+SOCIAL_AUTH_TWITTER_SECRET = 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'
